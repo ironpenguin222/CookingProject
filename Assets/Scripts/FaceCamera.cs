3d0f761cfd68094a80376d03293cdd707a2c5aff
ironpenguin222 <148375713+ironpenguin222@users.noreply.github.com>
2025-04-10 00:26:02 -0400

    Fixes

---
+++ Assets/Scripts/FaceCamera.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class FaceCamera : MonoBehaviour
+{
+    private Camera mainCamera;
+
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    void LateUpdate()
+    {
+        if (mainCamera == null) return;
+
+        transform.forward = mainCamera.transform.forward;
+    }
+}
